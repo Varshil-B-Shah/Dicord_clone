b92e0676b5f9d704038d46e06bb44cac8e80e179
Varshil-B-Shah <varshil4247shah@gmail.com>
2024-07-27 23:35:46 +0530

    managing members functionality added

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -85,7 +85,7 @@ export const EditServerMoadal = () => {
       <DialogContent aria-describedby="edit-server" className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Edit your server
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Give your server a personality with a name and an image. You can
