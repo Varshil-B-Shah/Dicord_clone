205500120b6c284a5d73349b00871aa9dd27d28e
Varshil-B-Shah <varshil4247shah@gmail.com>
2024-08-08 18:58:56 +0530

    messages api setup

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: [
-    "/",
-    "/(api(?!/socket)|trpc)(.*)",
-    "/((?!.*\\..*|_next|api/socket).*?)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
