c60cde23fd9e422d82cac08711521a32a17a4979
Varshil-B-Shah <varshil4247shah@gmail.com>
2024-08-08 20:25:23 +0530

    messages attachment added

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -11,29 +11,31 @@ import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
 
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    },[])
-    
-    if(!isMounted) {
-        return null
-    }
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    return (
-        <>
-            <CreateServerModal />
-            <InviteModal />
-            <EditServerMoadal />
-            <MembersModal />
-            <CreateChannelModal />
-            <LeaveServerModal />
-            <DeleteServerModal />
-            <DeleteChannelModal />
-            <EditChannelModal />
-        </>
-    )
-}
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+      <EditServerMoadal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      <MessageFileModal />
+    </>
+  );
+};
